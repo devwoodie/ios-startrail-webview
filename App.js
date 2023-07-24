29fc5a3682f92977f6db7549a7565c7f22d26671
devwoodie <ehddl453@naver.com>
2023-07-24 21:14:05 +0900

    feat: ios webview setting

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from 'react-native';
+import WebView from './components/WebView';
+
+const App = () => {
+
+  return (
+    <>
+      <SafeAreaView style={styles.root}>
+        <StatusBar barStyle={"light-content"} />
+        <WebView />
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: "#1E1E1E",
+    color: "#000"
+  },
+});
+
+export default App;
---
+++ components/WebView.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react';
+import {WebView} from 'react-native-webview';
+
+const MyWebView = ({route, navigation}) => {
+  const BASE_URL = 'https://www.byeoljachui.com/';
+  const [webview, setWebview] = useState();
+  useEffect(() => {
+    if (webview && webview.clearCache) webview.clearCache();
+  }, [webview]);
+
+  return (
+    <WebView
+      pullToRefreshEnabled={true}
+      startInLoadingState={true}
+      allowsBackForwardNavigationGestures={true}
+      source={{uri: BASE_URL}}
+      sharedCookiesEnabled={true}
+      mixedContentMode={'compatibility'}
+      originWhitelist={['https://*', 'http://*']}
+      overScrollMode={'never'}
+      onMessage={(event) => {
+
+        const message = event.nativeEvent.data;
+        console.log("event data : " + event.nativeEvent.data);
+
+        // if (message === "logout") {
+        //   navigation.navigate('Login');
+        //
+        //   AsyncStorage.removeItem('jwtKey');
+        // }
+
+      }}
+      // injectedJavaScript={`
+      //           (function() {
+      //               window.postMessage('${JSON.stringify(UserStore.getJwtKey)}', '*');
+      //           })();
+      //       `}
+    />
+  );
+};
+
+export default MyWebView;
